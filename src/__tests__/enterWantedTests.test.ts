37df7caf135720839937502a471f6a59aa899d83
JunTankai <87102438+JunTankai@users.noreply.github.com>
2021-08-18 20:10:13 -0700

    Create enterWantedTests.test.ts

---
+++ src/__tests__/enterWantedTests.test.ts
@@ -0,0 +1,7 @@
+const chromedriver = require("chromedriver");
+import { WebDriver, Builder, Capabilities } from "selenium-webdriver";
+const driver: WebDriver = new Builder()
+  .withCapabilities(Capabilities.chrome())
+  .build();
+
+import { PracticePage } from "./PracticePage";
